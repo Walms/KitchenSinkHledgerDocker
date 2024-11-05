3ac572f79e0f560c53c8035ea196e11b75c815ee
Daniel Walmsley <daniel.walmsley@amorini.co.nz>
2024-11-05 14:07:30 +1300

    Fix up kickstart to main repo

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ docker run -it --rm \
     -v "$(pwd)":/data \
     -v nvim-config:/root/.config \
     -v nvim-data:/root/.local/share/nvim \
-    -e KICKSTART_URL=https://github.com/Walms/kickstart.nvim.git \
+    -e KICKSTART_URL=https://github.com/nvim-lua/kickstart.nvim.git \
     hledger-dev
